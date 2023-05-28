9943b5cf1b8beb0ec9eedbaa513b1952aba86a47
elahemortazavi <emor5680@gmail.com>
2023-05-28 21:47:51 +0100

    app.js updated

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ShowContext from './ShowContext';
 import AddressContext from './AddressContext';
 import NumberContext from './NumberContext';
 import SoldCarsContext from './SoldCarsContext';
+import BestSeller from './Best Seller';
 
 export const NameContext = React.createContext();
 export const AgeContext = React.createContext();
@@ -83,9 +84,9 @@ function App(props) {
         <SoldCarsContext sold={sold} />
       </SoldContext.Provider>
 
-      <BestSold.Provider value={car}>
+      <BestSoldContext.Provider value={car}>
         <BestSeller car={car} />
-      </BestSold.Provider>
+      </BestSoldContext.Provider>
 
       <div>
         <button onClick={() => setToggle(!toggle)}>Toggle</button>
--- src/Best Seller.js
+++ src/Best Seller.js
@@ -2,10 +2,10 @@ import React, { useContext } from "react";
 import { BestSoldContext } from "./App";
 
 const BestSeller = () => {
-  const age = useContext(BestSoldContext);
+  const car = useContext(BestSoldContext);
   return (
     <div>
-      <h1>component BestSeller: {age}</h1>
+      <h1>component BestSeller: {car}</h1>
     </div>
   );
 };
